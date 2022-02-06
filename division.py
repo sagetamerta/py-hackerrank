c533c424a379dedeed56e2ae3ae9462470777483
Saget Amerta <sagetamerta@gmail.com>
2022-02-07 00:37:59 +0800

    division solved

---
+++ division.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    def intDivision(a, b):
+        return a // b
+
+    def floatDivision(a, b):
+        return a / b
+
+print(intDivision(a, b))
+print(floatDivision(a, b))
