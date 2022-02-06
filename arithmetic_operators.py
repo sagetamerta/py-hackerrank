1d468f20f4a27f3f8578521c29607db9c3a6c425
Saget Amerta <sagetamerta@gmail.com>
2022-02-07 00:14:49 +0800

    arithmetic operators solved

---
+++ arithmetic_operators.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    def summary(a, b):
+        return a + b
+
+    def difference(a, b):
+        return a - b
+
+    def times(a, b):
+        return a * b
+
+
+print(summary(a, b))
+print(difference(a, b))
+print(times(a, b))
